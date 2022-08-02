1b24d1bec9afd1f78ca38806a9ae191c16bc2162
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-02 17:02:20 +0200

    Add function_export NimSource class

---
+++ src/csharp/function_export/function_export.main/NimSource.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace function_export.main;
+
+public static class NimSource
+{
+    private const string NameDLL = "function_export";
+
+    [DllImport(NameDLL)]
+    public static extern void NimMain();
+
+    [DllImport(NameDLL)]
+    public static extern double add(double x, double y);
+
+    [DllImport(NameDLL)]
+    public static extern double cadd(double x, double y);
+
+    [DllImport(NameDLL)]
+    public static extern int get_name_size();
+
+    [DllImport(NameDLL)]
+    public static extern void get_name(StringBuilder buffer);
+}
