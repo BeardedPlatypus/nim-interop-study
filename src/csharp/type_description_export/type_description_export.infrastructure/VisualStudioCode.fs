3555b2152f7b57b9f0c3f43c7bf5ce184246fd4a
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-03 16:53:37 +0200

    Add type_description_export.infrastructure

---
+++ src/csharp/type_description_export/type_description_export.infrastructure/VisualStudioCode.fs
@@ -0,0 +1,10 @@
+﻿namespace type_description_export.infrastructure
+
+open System.Diagnostics
+
+module public VisualStudioCode =
+    let public start () : unit =
+        let processStartInfo = ProcessStartInfo("code", "folder ./nim/custom-types/")
+        processStartInfo.UseShellExecute <- true
+        Process.Start(processStartInfo) |> ignore
+
