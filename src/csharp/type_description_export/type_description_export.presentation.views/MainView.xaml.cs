99a5b0b608ab97eb0c82883d5eac4309a891f15f
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-03 08:13:52 +0200

    Add type_description_export view logic

---
+++ src/csharp/type_description_export/type_description_export.presentation.views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace type_description_export.presentation.views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : MetroWindow
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
