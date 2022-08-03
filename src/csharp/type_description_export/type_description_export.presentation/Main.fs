1a63dca1c079f30310bed7c38db9eadbd04f7609
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-03 08:14:54 +0200

    Add type_description_export application logic

---
+++ src/csharp/type_description_export/type_description_export.presentation/Main.fs
@@ -0,0 +1,23 @@
+﻿namespace type_description_export.presentation
+
+open Elmish.WPF
+
+module public Main =
+    type public Model = 
+      { todo : bool
+      }
+
+    [<RequireQualifiedAccess>]
+    type public CmdMsg =
+        | Initialize
+
+    type public Msg =
+        | NoOp
+
+    let public init (): Model * CmdMsg list = { todo = true }, [ CmdMsg.Initialize ]
+
+    let public update (msg: Msg) (model: Model) : Model * CmdMsg list =
+        match msg with 
+        | NoOp -> model, []
+
+    let bindings () : Binding<Model, Msg> list = []
