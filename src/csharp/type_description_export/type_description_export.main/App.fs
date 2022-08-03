602cdb2df42ef0efc6b770ed036322198bd9fc67
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-03 08:15:15 +0200

    Add type_description_export entry

---
+++ src/csharp/type_description_export/type_description_export.main/App.fs
@@ -0,0 +1,13 @@
+﻿open System
+open Elmish.WPF
+
+open type_description_export.presentation.views
+
+[<EntryPoint; STAThread>]
+let main _ =
+    Program.mkProgramWpfWithCmdMsg
+        type_description_export.presentation.Main.init 
+        type_description_export.presentation.Main.update 
+        type_description_export.presentation.Main.bindings
+        type_description_export.main.CmdMapping.toCmd
+    |> Program.runWindow (MainView())
---
+++ src/csharp/type_description_export/type_description_export.main/CmdMapping.fs
@@ -0,0 +1,16 @@
+﻿namespace type_description_export.main
+
+open Elmish
+
+module public CmdMapping =
+    open type_description_export.presentation.Main
+
+    let private initializeCmd () : Cmd<Msg> =
+        async {
+            do! Async.SwitchToThreadPool ()
+            return Msg.NoOp
+        } |> Cmd.OfAsync.result
+
+    let public toCmd (cmdMsg: CmdMsg) : Cmd<Msg> =
+        match cmdMsg with
+        | CmdMsg.Initialize -> initializeCmd ()  
