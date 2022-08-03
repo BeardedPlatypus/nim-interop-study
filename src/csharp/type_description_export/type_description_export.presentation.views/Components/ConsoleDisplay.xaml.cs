207bfe488149168dc0c4a4196c7649adee2b3435
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-03 16:58:39 +0200

    Add initial view components

---
+++ src/csharp/type_description_export/type_description_export.presentation.views/Components/ConsoleDisplay.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace type_description_export.presentation.views.Components;
+
+/// <summary>
+/// Interaction logic for ConsoleDisplay.xaml
+/// </summary>
+public partial class ConsoleDisplay : UserControl
+{
+    public ConsoleDisplay()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/csharp/type_description_export/type_description_export.presentation.views/Components/ObjectDisplay.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace type_description_export.presentation.views.Components;
+
+/// <summary>
+/// Interaction logic for ObjectDisplay.xaml
+/// </summary>
+public partial class ObjectDisplay : UserControl
+{
+    public ObjectDisplay()
+    {
+        InitializeComponent();
+    }
+}
