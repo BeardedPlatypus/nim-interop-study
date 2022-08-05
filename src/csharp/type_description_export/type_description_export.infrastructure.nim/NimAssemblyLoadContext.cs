49c226a2734cb1a9e1a6a5336b72dd7bda7ba0e6
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-05 15:04:26 +0200

    Add NimAssemblyLoadContext

---
+++ src/csharp/type_description_export/type_description_export.infrastructure.nim/NimAssemblyLoadContext.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace type_description_export.infrastructure.nim;
+
+public class NimAssemblyLoadContext : AssemblyLoadContext
+{
+    public NimAssemblyLoadContext() : base(isCollectible: true) { }
+    protected override Assembly? Load(AssemblyName name) => null;
+}
