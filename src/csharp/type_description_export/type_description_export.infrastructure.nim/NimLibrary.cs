4f4b29cffcdabe54b1c057d971adb9f82acbd6be
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-05 22:52:04 +0200

    Refactor NimLibrary to manage dll directly

--- src/csharp/type_description_export/type_description_export.infrastructure.nim/NimLibrary.cs
+++ src/csharp/type_description_export/type_description_export.infrastructure.nim/NimLibrary.cs
@@ -1,119 +1,120 @@
-﻿using System.Reflection;
+﻿using System.Runtime.InteropServices;
 using System.Text;
 
 namespace type_description_export.infrastructure.nim;
 
 public sealed class NimLibrary : IDisposable
 {
-    private readonly string _libraryPath;
-
-    private NimAssemblyLoadContext? _context = null;
-    private Assembly? _assembly = null;
-    private TypeDescriptionExportWrapper? _wrapper = null;
-    private bool _hasDisposed = false;
-
-    public NimLibrary()
+    private const string _libraryPath = "./nim/type_description_export.dll";
+    private Wrapper? _wrapper = null;
+    
+    private class Wrapper : IDisposable
     {
-        _libraryPath = Path.Combine(Directory.GetCurrentDirectory(), 
-                                    "nim", 
-                                    "type_description_export.infrastructure.nim.core.dll");
-    }
+        private readonly IntPtr _pDLL;
+        private readonly Delegates.NimMain _nimMain;
+        private readonly Delegates.get_n_components _getNComponents;
+        private readonly Delegates.get_component_name_length _getComponentNameLength;
+        private readonly Delegates.get_component_name _getComponentName;
+        private readonly Delegates.get_n_fields _getNFields;
+        private readonly Delegates.get_field_name_length _getFieldNameLength;
+        private readonly Delegates.get_field_name _getFieldName;
+        private readonly Delegates.get_field_type_index _getFieldTypeIndex;
+        private readonly Delegates.get_type_name_length _getTypeNameLength;
+        private readonly Delegates.get_type_name _getTypeName;
 
-    private class TypeDescriptionExportWrapper
-    {
-        private readonly Action _initialize;
-        private readonly Action _free;
-        private readonly Action _nimMain;
-        private readonly Func<int> _getNComponents;
-        private readonly Func<int, int> _getComponentNameLength;
-        private readonly Action<int, StringBuilder> _getComponentName;
-        private readonly Func<int, int> _getNFields;
-        private readonly Func<int, int, int> _getFieldNameLength;
-        private readonly Action<int, int, StringBuilder> _getFieldName;
-        private readonly Func<int, int, int> _getFieldTypeIndex;
-        private readonly Func<int, int> _getTypeNameLength;
-        private readonly Action<int, StringBuilder> _getTypeName;
-
-        public TypeDescriptionExportWrapper(Type wrapperType)
+        public Wrapper()
         {
-            _initialize = CreateDelegate<Action>(wrapperType, "Initialize");
-            _free = CreateDelegate<Action>(wrapperType, "Free");
-            _nimMain = CreateDelegate<Action>(wrapperType, "NimMain");
-            _getNComponents = CreateDelegate<Func<int>>(wrapperType, "get_n_components");
-            _getComponentNameLength = CreateDelegate<Func<int, int>>(wrapperType, "get_component_name_length");
-            _getComponentName = CreateDelegate<Action<int, StringBuilder>>(wrapperType, "get_component_name");
-            _getNFields = CreateDelegate<Func<int, int>>(wrapperType, "get_n_fields");
-            _getFieldNameLength = CreateDelegate<Func<int, int, int>>(wrapperType, "get_field_name_length");
-            _getFieldName = CreateDelegate<Action<int, int, StringBuilder>>(wrapperType, "get_field_name");
-            _getFieldTypeIndex = CreateDelegate<Func<int, int, int>>(wrapperType, "get_field_type_index");
-            _getTypeNameLength = CreateDelegate<Func<int, int>>(wrapperType, "get_type_name_length");
-            _getTypeName = CreateDelegate<Action<int, StringBuilder>>(wrapperType, "get_type_name");
+            _pDLL = NativeLibrary.Load(_libraryPath);
+
+            _nimMain = GetDelegate<Delegates.NimMain>(nameof(Delegates.NimMain));
+            _getNComponents = GetDelegate<Delegates.get_n_components>(nameof(Delegates.get_n_components));
+            _getComponentNameLength = GetDelegate<Delegates.get_component_name_length>(nameof(Delegates.get_component_name_length));
+            _getComponentName = GetDelegate<Delegates.get_component_name>(nameof(Delegates.get_component_name));
+            _getNFields = GetDelegate<Delegates.get_n_fields>(nameof(Delegates.get_n_fields));
+            _getFieldNameLength = GetDelegate<Delegates.get_field_name_length>(nameof(Delegates.get_field_name_length));
+            _getFieldName = GetDelegate<Delegates.get_field_name>(nameof(Delegates.get_field_name));
+            _getFieldTypeIndex = GetDelegate<Delegates.get_field_type_index>(nameof(Delegates.get_field_type_index));
+            _getTypeNameLength = GetDelegate<Delegates.get_type_name_length>(nameof(Delegates.get_type_name_length));
+            _getTypeName = GetDelegate<Delegates.get_type_name>(nameof(Delegates.get_type_name));
         }
 
-        private static T CreateDelegate<T>(Type wrapperType, string methodName) where T : Delegate =>
-            wrapperType.GetMethod(methodName, BindingFlags.Public | BindingFlags.Static)?
-                       .CreateDelegate<T>(null)
-                ?? throw new InvalidOperationException();
+        private T GetDelegate<T>(string name) where T : Delegate
+        {
+            IntPtr address = NativeLibrary.GetExport(_pDLL, name);
+            return Marshal.GetDelegateForFunctionPointer<T>(address);
+        }
 
-        public void Initialize() => 
-            _initialize.Invoke();
+        public void NimMain() => _nimMain!.Invoke();
+        public int GetNumberComponents() => _getNComponents();
+        public int GetComponentNameLength(int indexComponent) => _getComponentNameLength(indexComponent);
+        public void GetComponentName(int indexComponent, StringBuilder buffer) => _getComponentName(indexComponent, buffer);
+        public int GetNumberFields(int indexComponent) => _getNFields(indexComponent);
+        public int GetFieldNameLength(int indexComponent, int indexField) => _getFieldNameLength(indexComponent, indexField);
+        public void GetFieldName(int indexComponent, int indexField, StringBuilder buffer) => _getFieldName(indexComponent, indexField, buffer);
+        public int GetFieldTypeIndex(int indexComponent, int indexField) => _getFieldTypeIndex(indexComponent, indexField);
+        public int GetTypeNameLength(int indexType) => _getTypeNameLength(indexType);
+        public void GetTypeName(int indexType, StringBuilder buffer) => _getTypeName(indexType, buffer);
 
-        public void Free() =>
-            _free.Invoke();
+        private static class Delegates
+        {
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate void NimMain();
 
-        public void NimMain() => 
-            _nimMain.Invoke();
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate int get_n_components();
 
-        public int GetNumberComponents() => 
-            _getNComponents.Invoke();
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate int get_component_name_length(int component_index);
 
-        public int GetComponentNameLength(int componentIndex) => 
-            _getComponentNameLength.Invoke(componentIndex);
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate void get_component_name(int component_index, StringBuilder buffer);
 
-        public void GetComponentName(int componentIndex, StringBuilder buffer) =>
-            _getComponentName(componentIndex, buffer);
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate int get_n_fields(int component_index);
 
-        public int GetNumberFields(int componentIndex) =>
-            _getNFields.Invoke(componentIndex);
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate int get_field_name_length(int component_index, int field_index);
 
-        public int GetFieldNameLength(int componentIndex, int fieldIndex) =>
-            _getFieldNameLength.Invoke(componentIndex, fieldIndex);
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate void get_field_name(int component_index, int field_name, StringBuilder buffer);
 
-        public void GetFieldName(int componentIndex, int fieldIndex, StringBuilder buffer) =>
-            _getFieldName.Invoke(componentIndex, fieldIndex, buffer);
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate int get_field_type_index(int component_index, int field_name);
 
-        public int GetFieldTypeIndex(int componentIndex, int fieldIndex) =>
-            _getFieldTypeIndex(componentIndex, fieldIndex);
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate int get_type_name_length(int type_index);
 
-        public int GetTypeNameLength(int typeIndex) =>
-            _getTypeNameLength(typeIndex);
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate void get_type_name(int type_index, StringBuilder buffer);
+        }
 
-        public void GetTypeName(int typeIndex, StringBuilder buffer) =>
-            _getTypeName(typeIndex, buffer);
+        private void ReleaseUnmanagedResources() => NativeLibrary.Free(_pDLL);
+
+        public void Dispose()
+        {
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+
+        ~Wrapper() => ReleaseUnmanagedResources();
     }
 
     public bool CanLoad => !HasLoaded && File.Exists(_libraryPath);
-    public bool HasLoaded => _context != null;
+    public bool HasLoaded => _wrapper != null;
 
     public void Load()
     {
         if (!CanLoad) return;
-
-        _context = new NimAssemblyLoadContext();
-        _assembly = _context.LoadFromAssemblyPath(_libraryPath);
+        _wrapper = new Wrapper();
     }
 
     public void Unload()
     {
         if (!HasLoaded) return;
 
-        var contextWeakRef = new WeakReference(_context, trackResurrection: true);
-        _wrapper!.Free();
+        var contextWeakRef = new WeakReference(_wrapper, trackResurrection: true);
+        _wrapper!.Dispose();
         _wrapper = null;
-        _assembly = null;
-
-        _context?.Unload();
-        _context = null;
 
         // Ensure the _context has been garbage collected
         // See: https://docs.microsoft.com/en-us/dotnet/standard/assembly/unloadability#use-a-custom-collectible-assemblyloadcontext
@@ -124,14 +125,7 @@ public sealed class NimLibrary : IDisposable
         }
     }
 
-    public void Initialize()
-    {
-        // Assumption: Initialize is only called when the NimLibrary is loaded.
-        var typeDescriptionExportType = _assembly?.GetType("type_description_export.infrastructure.nim.core.TypeDescriptionExport") ?? throw new InvalidOperationException();
-        _wrapper = new TypeDescriptionExportWrapper(typeDescriptionExportType);
-        _wrapper.Initialize();
-        _wrapper.NimMain();
-    }
+    public void Initialize() => _wrapper!.NimMain();
 
     public IEnumerable<NimTypeDescription> GetComponents()
     {
@@ -186,13 +180,6 @@ public sealed class NimLibrary : IDisposable
 
     public void Dispose()
     {
-        if (!_hasDisposed)
-        {
-            Unload();
-            _hasDisposed = true;
-        }
-
-        GC.SuppressFinalize(this);
+        _wrapper?.Dispose();
     }
-
 }
