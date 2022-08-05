9e50922a208fb8c6000b2ae568d74678ab841605
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-05 15:04:12 +0200

    Add nim field and type descriptions

---
+++ src/csharp/type_description_export/type_description_export.infrastructure.nim/NimFieldDescription.cs
@@ -0,0 +1,3 @@
+﻿namespace type_description_export.infrastructure.nim;
+
+public record NimFieldDescription(string Name, string FieldType);
---
+++ src/csharp/type_description_export/type_description_export.infrastructure.nim/NimTypeDescription.cs
@@ -0,0 +1,3 @@
+﻿namespace type_description_export.infrastructure.nim;
+
+public record NimTypeDescription(string Name, IReadOnlyList<NimFieldDescription> Fields);
