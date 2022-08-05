a0644855baf3664f5953bc33bb4b0ab854e10880
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-05 16:16:54 +0200

    Add a common project

---
+++ src/csharp/type_description_export/type_description_export.common/Records.fs
@@ -0,0 +1,12 @@
+﻿namespace type_description_export.common
+
+module Records =
+    type public FieldDescription = 
+      { fieldName : string
+        typeName : string
+      }
+    type ComponentDescription = 
+      { componentName : string
+        fields : FieldDescription list
+      }
+
