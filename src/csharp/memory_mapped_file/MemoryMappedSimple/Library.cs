2e5755b83dec451140d971771d2df1b0d00fc881
BeardedPlatypus <mwtegelaers@gmail.com>
2022-08-09 11:17:36 +0200

    Add MemoryMappedSimple C# project

---
+++ src/csharp/memory_mapped_file/MemoryMappedSimple/Library.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace memory_mapped_file;
+
+public static class Library
+{
+    private const string Name = "memory_mapping_core";
+
+    [DllImport(Name)]
+    public static extern void NimMain();
+
+    [DllImport(Name)]
+    public static extern void Initialize();
+
+    [DllImport(Name)]
+    public static extern void Free();
+
+    [DllImport(Name)]
+    public static extern void Write(int v);
+
+    [DllImport(Name)]
+    public static extern int Read();
+}
---
+++ src/csharp/memory_mapped_file/MemoryMappedSimple/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO.MemoryMappedFiles;
+using memory_mapped_file;
+
+Library.NimMain();
+Library.Initialize();
+
+var mmf = MemoryMappedFile.OpenExisting("Local\\NimBuffer", MemoryMappedFileRights.ReadWriteExecute, HandleInheritability.Inheritable);
+using var accessor = mmf.CreateViewAccessor(0, sizeof(int));
+
+Library.Write(13);
+
+accessor.Read(0, out Int32 v);
+Console.WriteLine($"Nim says: {v}");
+
+Library.Free();
